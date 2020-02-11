eee4195c2834ef665d2bd6cac932d0320d9a7085
David Hein <david-hein196@web.de>
2020-02-11 14:32:15 +0100

    Reduce imports Implement tests

--- src/test/TestExperiment.java
+++ src/test/TestExperiment.java
@@ -1,13 +1,13 @@
 package test;
 
-import main.human_resources.IResearchGroup;
-import main.human_resources.ResearchGroup;
-import main.human_resources.Researcher;
-import main.infrastructure.ControlCenter;
-import main.infrastructure.IControlCenter;
-import main.infrastructure.IWorkplace;
-import main.infrastructure.Workplace;
+import main.human_resources.*;
+import main.infrastructure.*;
+import main.infrastructure.lhc.*;
+import main.infrastructure.lhc.detector.*;
+import main.infrastructure.lhc.experiment.*;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -52,28 +52,89 @@ public class TestExperiment {
 
     @Test
     @Order(2)
-    @DisplayName("")
+    @DisplayName("Check if all search algorithms work")
     public void detectorInitialized() {
+        Configuration config = Configuration.instance;
+        Detector detector;
 
+        config.searchAlgorithm = SearchAlgorithm.Native;
+        detector = new Detector();
+        detector.receive(new AnalyseEvent());
+        assertNotEquals(detector.getPort(), null);
+
+        config.searchAlgorithm = SearchAlgorithm.BoyerMoore;
+        detector = new Detector();
+        detector.receive(new AnalyseEvent());
+        assertNotEquals(detector.getPort(), null);
+
+        config.searchAlgorithm = SearchAlgorithm.KnuthMorrisPratt;
+        detector = new Detector();
+        detector.receive(new AnalyseEvent());
+        assertNotEquals(detector.getPort(), null);
     }
 
     @Test
+    @ParameterizedTest(name = "{index} => initialEnergy={0}, b={1}, sum={2}")
+    @CsvSource({
+            "1, 1, 6",
+            "2, 3, 5"
+    })
     @Order(3)
     @DisplayName("")
-    public void ringInitialisation() {
+    public void ringInitialisation(int initialEnergy) {
+        /*
+        ProtonTrap trap1 = new ProtonTrap(ProtonTrapID.A);
+        ProtonTrap trap2 = new ProtonTrap(ProtonTrapID.B);
 
+        Ring ring = new Ring();
+        Detector detector = new Detector();
+        ControlCenter controlCenter = ControlCenter.instance;
+
+        ring.setProtonTraps(trap1, trap2);
+        ring.setDetector(detector);
+
+        controlCenter.addSubscriber(ring);
+        controlCenter.addSubscriber(detector);
+
+        controlCenter.startExperiment(initialEnergy);*/
+        // TODO
     }
 
     @Test
     @Order(4)
-    @DisplayName("")
+    @DisplayName("Test if all proton traps are initialized correctly")
     public void protonTrapInitialisation() {
+        int protCount = 0;
+        ProtonTrap trap1 = new ProtonTrap(ProtonTrapID.A);
+        ProtonTrap trap2 = new ProtonTrap(ProtonTrapID.B);
+        Proton proton1;
+        Proton proton2;
 
+        do {
+            proton1 = trap1.release();
+            proton2 = trap2.release();
+            if (proton1 != null && proton2 != null) {
+                protCount++;
+                int[][][] struct1 = proton1.getStructure();
+                int[][][] struct2 = proton1.getStructure();
+                assertEquals(struct1.length, 100);
+                assertEquals(struct2.length, 100);
+                for (int i = 0; i < 100; i++) {
+                    assertEquals(struct1[i].length, 100);
+                    assertEquals(struct2[i].length, 100);
+                    for (int j = 0; j < 100; j++) {
+                        assertEquals(struct1[i][j].length, 100);
+                        assertEquals(struct2[i][j].length, 100);
+                    }
+                }
+            }
+        } while (proton1 != null && proton2 != null);
+        assertEquals(protCount, 25);
     }
 
     @Test
     @Order(5)
-    @DisplayName("")
+    @DisplayName("Check if first proton is taken from trap")
     public void takeFirstProtonInExperiment() {
 
     }
@@ -87,15 +148,49 @@ public class TestExperiment {
 
     @Test
     @Order(7)
-    @DisplayName("")
+    @DisplayName("Test if experiment is successful initialized")
     public void experimentSuccessfullyInitialized() {
+        ProtonTrap trap1 = new ProtonTrap(ProtonTrapID.A);
+        ProtonTrap trap2 = new ProtonTrap(ProtonTrapID.B);
 
+        Ring ring = new Ring();
+        Detector detector = new Detector();
+        ControlCenter controlCenter = ControlCenter.instance;
+
+        ring.setProtonTraps(trap1, trap2);
+        ring.setDetector(detector);
+
+        controlCenter.addSubscriber(ring);
+        controlCenter.addSubscriber(detector);
+
+        controlCenter.startExperiment(50, ExperimentScope.ES5);
+        IExperiment experiment = detector.getExperiments().get(0);
+
+        for (int i = 0; i < 200000; i++) {
+            assertEquals(experiment.getBlock(i).getStructure().length(), 10);
+        }
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> { experiment.getBlock(200000); });
     }
 
     @Test
     @Order(8)
-    @DisplayName("")
+    @DisplayName("Check if higgs was found")
     public void higgsFound() {
+        ProtonTrap trap1 = new ProtonTrap(ProtonTrapID.A);
+        ProtonTrap trap2 = new ProtonTrap(ProtonTrapID.B);
 
+        Ring ring = new Ring();
+        Detector detector = new Detector();
+        ControlCenter controlCenter = ControlCenter.instance;
+
+        ring.setProtonTraps(trap1, trap2);
+        ring.setDetector(detector);
+
+        controlCenter.addSubscriber(ring);
+        controlCenter.addSubscriber(detector);
+
+        controlCenter.startExperiment(50);
+
+        assertEquals(detector.getExperiments().get(16).getHiggsBosonFound(), true);
     }
 }
